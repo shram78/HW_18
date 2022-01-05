b30f29390a83bb1977e36826fca55ab91bfee815
Aleksandr <ra00275@gmail.com>
2022-01-05 21:23:28 +0300

    homework complete

---
+++ HW18.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+class Animal
+{
+public:
+	virtual void Voice()
+	{
+		std::cout << "Welcome to the zoo\n";
+	}
+};
+
+class Dog : public Animal
+{
+	void Voice() override
+	{
+		std::cout << "Woof!\n";
+	}
+};
+
+class Cat : public Animal
+{
+	void Voice() override
+	{
+		std::cout << "Meow!\n";
+	}
+};
+
+class Cow : public Animal
+{
+	void Voice() override
+	{
+		std::cout << "Moo!\n";
+	}
+};
+
+int main()
+{
+	Animal* animals[] = { {new Animal}, {new Dog}, {new Cat}, {new Cow} };
+
+	for (Animal* animal : animals)
+		animal->Voice();
+}
